f1ff857b19e0211d85a0b9562b848880e2daec75
jaayzee <43892735+jaayzee@users.noreply.github.com>
2018-10-24 12:25:56 -0700

    catchup

--- Main.java
+++ Main.java
@@ -184,22 +184,67 @@ class Main
         /*
          * Task 9. In the array ar2, count how many odd numbers you
          * have.  Then create an ew array called ar4.  Copy just the odd
-         * numbers from ar1 into ar4.  Print ar4
+         * numbers from ar2 into ar4.  Print ar4
          */
-        
+         
+         // Find out how big ar4 should be
+         int l4=0;
+        for (int i=0 ; i<ar2.length ; i++)
+        {
+            if ( ar2[i]%2==1)
+            {
+                l4++;
+            }
+        } 
+         
+         // Create a new array of exactly the right size
+         int[] ar4 = new int[l4];
+         int k=0;
+        for (int i=0 ; i<ar2.length ; i++)
+        {
+            if (ar2[i]%2==1)
+            {
+                ar4[k]=ar2[i];
+                k++;
+            }
+        }           
+        for (int i=0 ; i<k ; i++)
+        {
+               System.out.println(ar4[i]);
+        }    
         /*
          * Task 10.  Shift the elements of ar4 right by 1
          * For example
          * old   ar4: 1 3 5 7 9
          * new   ar4  9 1 3 5 7
          */
-        
+        System.out.println("** Task 10 ** ");
+        System.out.println("Original ar4");
+        for (int i=0 ; i<ar4.length ; i++)
+        {
+            System.out.println(ar4[i]);
+        }  
+        int temp10=ar4[ar4.length-1];
+        for (int i=ar4.length-1 ; i>0 ; i--)
+        {
+           ar4[i]=ar4[i-1];
+        } 
+        ar4[0]=temp10; 
         
         /*
          * Task 11.  Reverse the order of elements in ar2
          */
-        
-        
+        System.out.println("** Task 11 ** ");
+        int[] news = new int[ar2.length];
+        for (int i=0 ; i<(ar2.length) ; i++)
+        {
+            news[i] = ar2[ar2.length-i-1];
+        }
+        System.out.println("New ar2");
+        for (int i=0 ; i<ar2.length ; i++)
+        {
+            System.out.println(news[i]);
+        }
         /*
          * Task 12: 
          * Create an array of Strings called ar5.
